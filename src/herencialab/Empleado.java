99d8357f7307cfc543ad275feebb355853f8125c
Samuel Vasquez <83726280+Draccoo222@users.noreply.github.com>
2025-08-01 08:56:18 -0600

    empleadoBase

--- src/herencialab/Empleado.java
+++ src/herencialab/Empleado.java
@@ -4,10 +4,68 @@
  */
 package herencialab;
 
+import java.util.Calendar;
+
 /**
  *
  * @author unwir
  */
 public class Empleado {
+    private int code;
+    private int horasChamba;
+    private String nombre;
+    private Calendar fechaCreacion;
+    private double salarioBase;
     
+    
+    public Empleado(int code, String nombre, double salarioBase){
+        this.code = code;
+        horasChamba = 0;
+        this.nombre = nombre;
+        this.salarioBase = salarioBase;
+        fechaCreacion = Calendar.getInstance();
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public int getHorasChamba() {
+        return horasChamba;
+    }
+
+    public void registrarHorasTrabajadas(int horasChamba) {
+        this.horasChamba += horasChamba;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Calendar getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+    
+    
+    public double calcularPago(double horasChamba){
+       return ((horasChamba/160) * this.salarioBase) * 0.965;
+    }
+    
+    @Override
+    public String toString(){
+        return "Codigo: " + code +
+               "\nNombre: " + nombre +
+               "\nHoras Trabajadas: " + horasChamba + 
+               "\nFecha de Entrada: " + fechaCreacion.getTime() +
+               "\nSalario Base: " + salarioBase;
+    }
+
 }
