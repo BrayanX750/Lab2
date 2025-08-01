7c30afd0d40f340bf1c0a5bf507ee03c06f2d2a1
BrayanX750 <brayan_pinel@unitec.edu>
2025-08-01 09:17:53 -0600

    Clase EmpleadoTemporal

---
+++ src/herencialab/EmpleadoTemporal.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package herencialab;
+
+import java.util.Calendar;
+
+public class EmpleadoTemporal extends Empleado {
+    
+private Calendar fechaFinContrato;
+public EmpleadoTemporal(int code, String nombre,double salarioBase, Calendar fechaFinContrato){
+    super(code,nombre,salarioBase);
+    this.fechaFinContrato=fechaFinContrato;
+}
+
+@Override
+public double calcularPago(double horasChamba){
+    Calendar hoy=Calendar.getInstance();
+    if(!hoy.after(fechaFinContrato)){
+        
+        return(horasChamba/160.0)*getSalarioBase();
+       
+        
+    }else{
+        return 0.0;
+    }
+}
+
+public void acctualizarFechaSinContrato(Calendar nuevaFecha){
+    this.fechaFinContrato=fechaFinContrato;
+}
+
+@Override
+public String toString(){
+    return super.toString()+
+            "/nFecha de fin de contrato: "+fechaFinContrato.getTime();
+    
+}
+
+public Calendar getFechaFinContrato(){
+    return fechaFinContrato;
+}
+
+}
